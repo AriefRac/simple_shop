7b75288b3af5309f2ed73eb82d95b384162f2d39
Arief <muhariefrac@gmail.com>
2025-04-28 10:52:48 +0700

    chore: make ALLOWED_HOSTS dynamic from environment variable

--- simple_shop/settings.py
+++ simple_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-^@+)n27iadi*3fe7sx&g8r@bvlidv7zs6f=4nyff8oe5!q)vx9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-f45a4.up.railway.app']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
 
 # Application definition
